e7881d9850eeb275585ce7cd7edef59ea2f6f71c
yenilikci <m.melihcelik017@outlook.com>
2021-07-31 05:06:59 +0300

    Create project with vue3

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="App">
+    <ImageGrid />
+  </div>
+</template>
+
+<script>
+import ImageGrid from "./components/ImageGrid.vue";
+export default {
+  name: "App",
+  components: {
+    ImageGrid,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ImageGrid.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Resim Grid</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
