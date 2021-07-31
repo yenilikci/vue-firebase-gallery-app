61704b08c0e906147d7aa76b9543c4d38440eaac
yenilikci <m.melihcelik017@outlook.com>
2021-07-31 15:47:28 +0300

    Add application title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="App">
+    <Title />
     <ImageGrid />
   </div>
 </template>
 
 <script>
 import ImageGrid from "./components/ImageGrid.vue";
+import Title from "./components/Title.vue";
 export default {
   name: "App",
   components: {
     ImageGrid,
+    Title,
   },
 };
 </script>
---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div className="title">
+    <h1>Gallery App</h1>
+    <p>Photo gallery with vue3 and firebase</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.title h1 {
+  color: red;
+  font-size: 2rem;
+  letter-spacing: 2px;
+  margin: 40px 0 10px 0;
+  text-align: center;
+}
+.title p {
+  text-align: center;
+  color: blue;
+}
+</style>
